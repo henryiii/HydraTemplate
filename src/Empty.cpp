630021f09f98b421ae2f56dfd917d55aa3958a66
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2016-11-19 18:07:20 +0100

    Cleaner files, one bug fixed

---
+++ src/Empty.cpp
@@ -0,0 +1,24 @@
+/*
+ *  Empty.cpp
+ *  When you change the name of this file, change it in the CMakeLists.txt in the same folder.
+ */
+
+#include <iostream>
+
+//this lib
+#include <hydra/Types.h>
+
+/**
+ * @file
+ * @example Empty.cpp
+ * @brief This is a simple file to start a new user on his code.
+ *
+**/
+
+/// Doxygen comments start with three slashes or two slashes and a star
+hydra::GInt_t main(int argv, char** argc) {
+    // Code
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
